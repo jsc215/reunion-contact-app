51b8328153438b871a19773e71b389abe69a7008
jsc215 <jeffcomertennis@gmail.com>
2018-06-07 05:55:40 -0400

    created the list which fetches all contacts from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { UserService } from './services/user.service';
+import { RegistrationValidator } from './shared/register.validator';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import {
   MatToolbarModule,
   MatButtonModule,
@@ -7,8 +11,12 @@ import {
   MatIconModule,
   MatListModule,
   MatCardModule,
-  MatInputModule
+  MatInputModule,
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule
 } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -16,16 +24,22 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { RegiFormComponent } from './regi-form/regi-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher, MatPaginator } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { TableModule } from 'primeng/table';
+
 const appRoutes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegiFormComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegiFormComponent],
+  declarations: [AppComponent, LoginComponent, RegiFormComponent, HomeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    TableModule,
     MatToolbarModule,
     MatButtonModule,
     LayoutModule,
@@ -34,11 +48,18 @@ const appRoutes: Routes = [
     MatListModule,
     MatCardModule,
     MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     FormsModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher },
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatSortable, MatTableDataSource } from '@angular/material';
+import { User } from '../models/User';
+import { UserService } from '../services/user.service';
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  // currentUser: User;
+  dataSource;
+  displayedColumns = ['firstName', 'lastName', 'email'];
+  users: User[] = [];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private userService: UserService) {
+    // this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((users) => {
+      const user = Array.from((this.users = users['users'].map((mapUser) => mapUser)));
+      this.dataSource = new MatTableDataSource(user);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  deleteUser(_id: string) {
+    this.userService.delete(_id).subscribe((res) => {
+      return this.getUsers();
+    });
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe((users) => {
+      const user = Array.from((this.users = users['users'].map((mapUser) => mapUser)));
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
